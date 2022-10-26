9e1f3a6c089331ebf2e678bd509e3853900d8bf7
Yingjie <yingjie@zipcoders-mbp.myfiosgateway.com>
2022-10-25 22:19:36 -0400

    Update ReadMe with answer

--- TicketMachine.java
+++ TicketMachine.java
@@ -1,98 +1,91 @@
 /**
- * TicketMachine models a naive ticket machine that issues
+ * TicketMachine models a ticket machine that issues
  * flat-fare tickets.
  * The price of a ticket is specified via the constructor.
- * It is a naive machine in the sense that it trusts its users
- * to insert enough money before trying to print a ticket.
- * It also assumes that users enter sensible amounts.
- *
- * @author David J. Barnes and Michael Kolling
- * @version 2008.03.30
+ * Instances will check to ensure that a user only enters
+ * sensible amounts of money, and will only print a ticket
+ * if enough money has been input.
+ * @author David J. Barnes and Michael Kölling
+ * @version 2006.03.30
  */
-public class TicketMachine
-{
+public class TicketMachine {
     // The price of a ticket from this machine.
-    private Integer price;
-    // The amount of money entered by a customer so far.
-    private Integer balance;
-    // The total amount of money collected by this machine.
-    private Integer total;
-    // The number of tickets printed.
-    private Integer ticketNumber;
+    private int price;
+    // The amount of money entered by a customer so far. 
+    private int balance;
+    // The total amount of money collected by this machine. 
+    private int total;
+    // The discount from this machine.
+    private int discount;
+    // the saving will calculate the amount after the discount.
+    private int saving;
+/**
+ * Create a machine that issues tickets of the given price.
+ */
+public TicketMachine(int ticketCost) {
+    price = ticketCost;
+    balance = 0;
+    total = 0;
+}
 
-    /**
-     * Create a machine that issues tickets of the given price.
-     * Note that the price must be greater than zero, and there
-     * are no checks to ensure this.
-     */
-    public TicketMachine(Integer ticketCost)
-    {
-        price = ticketCost;
-        balance = 0;
-        total = 0;
-        ticketNumber = 0;
-    }
+/**
+ * Return the price of a ticket.
+ */
+public int getPrice() {
+    return price; 
+}
 
+/**
+ * Return the amount of money already inserted for the
+ * next ticket.
+ */
+public int getBalance() {
+    return balance; 
+}
+/**
+ * Receive an amount of money in cents from a customer.
+ * Check that the amount is sensible.
+ */
+public void insertMoney(int amount) {
+    if(amount > 0) {
+    balance = balance + amount;
+    }
+    else {
+            System.out.println("Use a positive amount: " +
+                               amount);
+    } 
+}
+/**
+ * Print a ticket if enough money has been inserted, and
+ * reduce the current balance by the ticket price. Print
+ * an error message if more money is required.
+ */
+public void printTicket() {
+    int amountLeftToPay=price-balance;
+        if(amountLeftToPay <= 0) {
+    // Simulate the printing of a ticket. System.out.println("##################"); System.out.println("# The BlueJ Line"); System.out.println("# Ticket"); System.out.println("# " + price + " cents."); System.out.println("##################"); System.out.println();
+                // Update the total collected with the price.
+                total = total + price;
+                // Reduce the balance by the price.
+                balance = balance - price;
+            }
+    else {
+    System.out.println("You must insert at least: " +
+                                   amountLeftToPay + " cents.");
+    } 
+}
     /**
-     * Return the price of a ticket.
+     * Return the money in the balance.
+     * The balance is cleared.
      */
-    public Integer getPrice()
-    {
-        return price;
-    }
-    
-    /**
-     * Return ticketNumber.
-     * (Increments on each print.)
-     */
-    public Integer getTicketNumber()
-    {
-        return ticketNumber;
-    }
-
-    /**
-     * Return the amount of money already inserted for the
-     * next ticket.
-     */
-    public Integer getBalance()
-    {
-        return balance;
-    }
-
-    /**
-     * Receive an amount of money in cents from a customer.
-     */
-    public Integer insertMoney(Integer amount)
-    {
-        balance = balance + amount;
-        return balance;
-    }
-    
-    public Integer calculateTotal(){
-        total = balance + total;
-        return total;
-    }
-    
-    public Integer incrementTicketNumber(){
-        ticketNumber++;
-        return ticketNumber;
-    }
-    
-
-    /**
-     * Print a ticket.
-     * Update the total collected and
-     * reduce the balance to zero.
-     */
-    public String printTicket()
-    {
-        //Increment the number of tickets printed
-        incrementTicketNumber();
-        // Update the total collected with the balance.
-        total = total + balance;
-        // Clear the balance.
-        balance = 0;
-        
-        return "Ticket price: " + price + " cents. " + "Your total is " + total + ".";
-    }
+public int refundBalance() {
+    int amountToRefund; 
+    amountToRefund = balance; 
+    balance = 0;
+    return amountToRefund;
+} 
+public int emptyMachine(){
+    int total =0;
+    return total;
+}
 }
